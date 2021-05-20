aacc1399ccf80fbe4a2949de7fed7abbad68d1f3
George Harris <citizenharris@gmail.com>
2021-05-20 14:15:44 +0100

    Add endpoint that always returns true

--- src/main/java/com/codurance/sessionize/sessionizeservice/HelloController.java
+++ src/main/java/com/codurance/sessionize/sessionizeservice/HelloController.java
@@ -1,14 +1,32 @@
 package com.codurance.sessionize.sessionizeservice;
 
+import com.codurance.sessionize.sessionizeservice.authentication.AuthenticationController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-  @GetMapping("/hello")
-  public String hello() {
-    return "Hello, World!";
-  }
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 
+    @PostMapping("/isNewUser")
+    public boolean isNewUser(SlackUserIdentity user) {
+        logger.info(user.id);
+        logger.info(user.name);
+        logger.info(user.email);
+        return true;
+    }
+
+    private class SlackUserIdentity {
+        String id;
+        String name;
+        String email;
+    }
 }
