d6e1f48aaf2007dc1da93ce2d427b12709d150e9
anddak <andras.dako@gmail.com>
2021-05-21 15:14:09 +0100

    change User id from UUID to String for Spring Data @Id and mongodb default is String

--- src/main/java/com/codurance/sessionize/sessionizeservice/user/User.java
+++ src/main/java/com/codurance/sessionize/sessionizeservice/user/User.java
@@ -5,15 +5,13 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.UUID;
-
 @Getter
 @Setter
 @Document(collection = "users")
 public class User {
 
   @Id
-  private UUID   id;
+  private String id;
   private String email;
   private String pictureURL;
   private String firstName;
