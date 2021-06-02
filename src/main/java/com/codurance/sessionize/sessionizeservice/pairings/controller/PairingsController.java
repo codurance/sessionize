dd1f51816c749586cb072217fe096d986cc45470
George Harris <citizenharris@gmail.com>
2021-06-02 09:54:58 +0100

    Add get pairings endpoint for slackbot

--- src/main/java/com/codurance/sessionize/sessionizeservice/pairings/controller/PairingsController.java
+++ src/main/java/com/codurance/sessionize/sessionizeservice/pairings/controller/PairingsController.java
@@ -5,15 +5,11 @@ import com.codurance.sessionize.sessionizeservice.pairings.service.PairingsServi
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static com.codurance.sessionize.sessionizeservice.infrastructure.constants.HttpConstants.AUTH_HEADER;
-import static com.codurance.sessionize.sessionizeservice.infrastructure.constants.HttpConstants.PAIRINGS;
+import static com.codurance.sessionize.sessionizeservice.infrastructure.constants.HttpConstants.*;
 
 
 @RestController
@@ -27,14 +23,14 @@ public class PairingsController {
   }
 
   @GetMapping(value = PAIRINGS, produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<List<Pairing>> getPairings(@RequestHeader(AUTH_HEADER) String email) {
+  public ResponseEntity<List<Pairing>> getPairings(@RequestHeader(AUTH_HEADER) @RequestParam String email) {
     List<Pairing> pairings = pairingsService.getPairings(email);
     return new ResponseEntity<>(pairings, HttpStatus.OK);
   }
 
-  @GetMapping(value = "/pairings/test", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<List<Pairing>> getPairings() {
-    List<Pairing> pairings = pairingsService.getPairings("sophie.biber@codurance.com");
+  @GetMapping(value = SLACK + PAIRINGS, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<Pairing>> getPairingsForSlack(@RequestParam String email) {
+    List<Pairing> pairings = pairingsService.getPairings(email);
     return new ResponseEntity<>(pairings, HttpStatus.OK);
   }
 }
