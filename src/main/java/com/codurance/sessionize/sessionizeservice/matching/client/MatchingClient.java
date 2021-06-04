f2d72580468e12524cb3037a4fd3a94051e8ff36
anddak <andras.dako@gmail.com>
2021-06-04 10:52:21 +0100

    WIP slack call restructure

--- src/main/java/com/codurance/sessionize/sessionizeservice/matching/client/MatchingClient.java
+++ src/main/java/com/codurance/sessionize/sessionizeservice/matching/client/MatchingClient.java
@@ -5,6 +5,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -15,7 +16,7 @@ public class MatchingClient {
     RestTemplate restTemplate = new RestTemplate();
     private static final String MATCHING_URI = "https://pythonmatchertestfn.azurewebsites.net/api/SessionizeMatching?";
 
-    public List<MatchResponse> match(List<UserLanguagePreferences> userLanguagePreferences) {
+    public List<MatchResponse> match(List<UserLanguagePreferences> userLanguagePreferences) throws HttpServerErrorException {
 
         List<UserLanguagePreferencesRequest> preferencesRequest = preparePreferencesRequest(userLanguagePreferences);
         MatchRequest matchRequest = new MatchRequest(preferencesRequest);
---
+++ src/test/java/com/codurance/sessionize/sessionizeservice/matching/client/SlackClientShould.java
@@ -0,0 +1,9 @@
+package com.codurance.sessionize.sessionizeservice.matching.client;
+
+import org.junit.jupiter.api.Test;
+
+public class SlackClientShould {
+
+
+
+}
