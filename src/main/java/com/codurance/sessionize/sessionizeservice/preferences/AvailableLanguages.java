26f0336896757f499b616440ae07564cd32898f4
sb48 <sophiebiber@icloud.com>
2021-06-08 10:48:24 +0100

    Add more language options for user

--- src/main/java/com/codurance/sessionize/sessionizeservice/preferences/AvailableLanguages.java
+++ src/main/java/com/codurance/sessionize/sessionizeservice/preferences/AvailableLanguages.java
@@ -5,13 +5,43 @@ import java.util.List;
 
 public class AvailableLanguages {
 
-  public static List<Language> get() {
-    return Arrays.asList(
-      new Language("JAVA", "Java"),
-      new Language("CSHARP", "C#"),
-      new Language("GOLANG", "Go"),
-      new Language("CPP", "C++")
-    );
-  }
+    public static List<Language> get() {
+        return Arrays.asList(
+                new Language("JAVA", "Java"),
+                new Language("CSHARP", "C#"),
+                new Language("GOLANG", "Go"),
+                new Language("CPP", "C++"),
+                new Language("C", "C"),
+                new Language("RUST", "Rust"),
+                new Language("KOTLIN", "Kotlin"),
+                new Language("FSHARP", "F#"),
+                new Language("CLOJURE", "Clojure"),
+                new Language("PYTHON", "Python"),
+                new Language("JS", "JavaScript"),
+                new Language("TS", "TypeScript"),
+                new Language("SCALA", "Scala"),
+                new Language("HASKELL", "Haskell"),
+                new Language("RACKET", "Racket"),
+                new Language("RACKET", "Racket"),
+                new Language("CP", "Common Lisp"),
+                new Language("VB", "Visual Basic"),
+                new Language("RUBY", "Ruby"),
+                new Language("GROOVY", "Groovy"),
+                new Language("OCAML", "OCaml"),
+                new Language("PHP", "PHP"),
+                new Language("SCHEME", "Scheme"),
+                new Language("PERL", "Perl"),
+                new Language("SWIFT", "Swift"),
+                new Language("R", "R"),
+                new Language("FORTRAN", "Fortran"),
+                new Language("COBOL", "Cobol"),
+                new Language("PASCAL", "Pascal"),
+                new Language("DART", "Dart"),
+                new Language("JULIA", "Julia"),
+                new Language("ELIXIR", "Elixir"),
+                new Language("ELM", "Elm")
+
+        );
+    }
 
 }
